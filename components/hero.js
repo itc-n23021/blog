63273793ae4bead1dec633574e94992b92d0f0cb
itc-n23021 <n23021@std.it-college.ac.jp>
2023-12-15 04:13:41 +0000

    editted index.js hero.js

--- components/hero.js
+++ components/hero.js
@@ -3,7 +3,7 @@ const Hero = ({ title, subtitle, imageOn = false }) => {
     <div>
       <h1>{title}</h1>
       <p>{subtitle}</p>
-      {imageOn && <figure> [image] </figure>}
+      {imageOn && <figure> [画像] </figure>}
     </div>
   )
 }

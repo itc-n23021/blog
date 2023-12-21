8e9444086b002367aa27242c474c8184484f9d3c
itc-n23021 <n23021@std.it-college.ac.jp>
2023-12-21 08:29:47 +0000

    5.7

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import Layout from 'components/layout'
 import 'styles/globals.css'
 
+// Font Awesome の設定
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 const Myapp = ({ Component, pageProps }) => {
   return (
     <Layout>
